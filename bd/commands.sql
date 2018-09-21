5b5f332fe63e35b2b4ad2181c27ecc5f9625ec1c
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-20 22:11:47 -0300

    comandos para remover e alterar o usuario no banco

---
+++ bd/commands.sql
@@ -0,0 +1,16 @@
+INSERT INTO T_HTK_USUARIO (id_usuario, ds_email, cd_senha, nm_usuario, 
+   dt_nascimento, vl_altura, cd_genero, vl_limite_caloria, dt_ult_login) 
+ VALUES (1, 'teste@teste.com', 'senha123', 'Nome de teste', 
+   TO_DATE('01/01/1990','DD/MM/YYYY'), 1.7, 'M', 2000, NULL);
+
+UPDATE T_HTK_USUARIO 
+ SET
+  ds_email = 'joao@gmail.com', 
+  cd_senha = 'grandesertaoveredas', 
+  nm_usuario = 'João Guimarães Rosa', 
+  dt_nascimento = TO_DATE('27/06/1998','DD/MM/YYYY'), 
+  vl_altura = 1.7, 
+  cd_genero = 'M', 
+  vl_limite_caloria = 2000, 
+  dt_ult_login = TO_DATE('18/09/2018','DD/MM/YYYY')
+ WHERE id_usuario = 1;
