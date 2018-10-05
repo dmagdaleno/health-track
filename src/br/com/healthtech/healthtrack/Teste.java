b4c4b51eaabc127c9d25c1519b433625b015fade
dmagdaleno <diegommagdaleno@gmail.com>
2018-10-05 14:30:58 -0300

    testa a inserção no banco

--- src/br/com/healthtech/healthtrack/Teste.java
+++ src/br/com/healthtech/healthtrack/Teste.java
@@ -8,40 +8,39 @@ import java.time.format.DateTimeFormatter;
 import br.com.healthtech.healthtrack.dao.AlimentacaoDAO;
 import br.com.healthtech.healthtrack.modelo.Usuario;
 import br.com.healthtech.healthtrack.modelo.registro.Alimentacao;
-import br.com.healthtech.healthtrack.modelo.registro.TipoAlimentacao;
 
 public class Teste {
 
 	public static void main(String[] args) {		
 		AlimentacaoDAO dao = new AlimentacaoDAO();
-		adicionaRegistrosDeAlimentacao(dao);
-		
-//		System.out.println("Lista de alimentações registradas:");
-//		dao.getAll().forEach(alimentacao -> System.out.println(alimentacao));
+//		adicionaRegistrosDeAlimentacao(dao);
+		System.out.println("Lista de alimentações registradas:");
+		dao.getAll().forEach(alimentacao -> System.out.println(alimentacao));
+		dao.fechaConexao();
 	}
 
 	private static void adicionaRegistrosDeAlimentacao(AlimentacaoDAO dao) {
 		Usuario usuario = constroiUsuario();
 		
-		dao.add(new Alimentacao(1L, TipoAlimentacao.CAFE_DA_MANHA, "Cereal, iogurte e mel", 
+		dao.add(new Alimentacao(1L, 1, "Cereal, iogurte e mel", 
 				new BigDecimal(350), novaData("10/09/2018 07:00"), usuario));
-		dao.add(new Alimentacao(2L, TipoAlimentacao.FRUTA, "Uma maçã grande", 
+		dao.add(new Alimentacao(2L, 2, "Uma maçã grande", 
 				new BigDecimal(100), novaData("10/09/2018 09:30"), usuario));
-		dao.add(new Alimentacao(3L, TipoAlimentacao.ALMOCO, "Arroz, feijão, carne e salada", 
+		dao.add(new Alimentacao(3L, 3, "Arroz, feijão, carne e salada", 
 				new BigDecimal(650), novaData("10/09/2018 12:00"), usuario));
-		dao.add(new Alimentacao(4L, TipoAlimentacao.LANCHE_LEVE, "Lanche", 
+		dao.add(new Alimentacao(4L, 4, "Lanche", 
 				new BigDecimal(220), novaData("10/09/2018 16:00"), usuario));
-		dao.add(new Alimentacao(5L, TipoAlimentacao.JANTAR, "Filé de frango e salada", 
+		dao.add(new Alimentacao(5L, 5, "Filé de frango e salada", 
 				new BigDecimal(580), novaData("10/09/2018 19:00"), usuario));
-		dao.add(new Alimentacao(6L, TipoAlimentacao.CAFE_DA_MANHA, "Cereal, iogurte e mel", 
+		dao.add(new Alimentacao(6L, 1, "Cereal, iogurte e mel", 
 				new BigDecimal(350), novaData("11/09/2018 07:00"), usuario));
-		dao.add(new Alimentacao(7L, TipoAlimentacao.FRUTA, "Uma maçã grande", 
+		dao.add(new Alimentacao(7L, 2, "Uma maçã grande", 
 				new BigDecimal(100), novaData("11/09/2018 09:30"), usuario));
-		dao.add(new Alimentacao(8L, TipoAlimentacao.ALMOCO, "Arroz, feijão, carne e salada", 
+		dao.add(new Alimentacao(8L, 3, "Arroz, feijão, carne e salada", 
 				new BigDecimal(650), novaData("11/09/2018 12:00"), usuario));
-		dao.add(new Alimentacao(9L, TipoAlimentacao.LANCHE_LEVE, "Lanche", 
+		dao.add(new Alimentacao(9L, 4, "Lanche", 
 				new BigDecimal(220), novaData("11/09/2018 16:00"), usuario));
-		dao.add(new Alimentacao(10L, TipoAlimentacao.JANTAR, "Filé de frango e salada", 
+		dao.add(new Alimentacao(10L, 5, "Filé de frango e salada", 
 				new BigDecimal(580), novaData("11/09/2018 19:00"), usuario));
 	}
 	
