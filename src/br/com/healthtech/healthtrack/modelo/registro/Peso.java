11b34a24e374b9cdb1071fc465eac882d1931750
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-10-05 20:52:27 -0300

    tostring equals e hashcode

--- src/br/com/healthtech/healthtrack/modelo/registro/Peso.java
+++ src/br/com/healthtech/healthtrack/modelo/registro/Peso.java
@@ -43,5 +43,34 @@ public class Peso extends Registro {
 	public LocalDateTime getDataRegistro() {
 		return data;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Peso [peso=" + peso + ", id=" + id + ", usuario=" + usuario + ", data=" + data + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((peso == null) ? 0 : peso.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peso other = (Peso) obj;
+		if (peso == null) {
+			if (other.peso != null)
+				return false;
+		} else if (!peso.equals(other.peso))
+			return false;
+		return true;
+	}
 }
