74553e68add0c97cf223c52c19295df902a280af
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-11-19 12:19:28 -0200

    utilizando a classe factory para definir a implementação

--- src/br/com/healthtech/healthtrack/dao/DAOFactory.java
+++ src/br/com/healthtech/healthtrack/dao/DAOFactory.java
@@ -1,9 +1,11 @@
 package br.com.healthtech.healthtrack.dao;
 
+import br.com.healthtech.healthtrack.dao.impl.PesoDAOOracle;
+
 public class DAOFactory {
 
   public static PesoDAO getPesoDAO() {
-    return new PesoDAO();
+    return new PesoDAOOracle();
   }
   
   public static PressaoArterialDAO getPressaoArterialDAO() {
