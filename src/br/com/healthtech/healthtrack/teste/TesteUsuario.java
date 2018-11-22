370bc022680d96208712adea59aeba27f83c3b24
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-11-21 23:07:53 -0200

    testa inserção de usuarios

---
+++ src/br/com/healthtech/healthtrack/teste/TesteUsuario.java
@@ -0,0 +1,43 @@
+package br.com.healthtech.healthtrack.teste;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import br.com.healthtech.healthtrack.dao.DAOFactory;
+import br.com.healthtech.healthtrack.dao.UsuarioDAO;
+import br.com.healthtech.healthtrack.exception.DBException;
+import br.com.healthtech.healthtrack.modelo.Login;
+import br.com.healthtech.healthtrack.modelo.Usuario;
+import br.com.healthtech.healthtrack.utils.DateUtil;
+
+public class TesteUsuario {
+	
+	public static void main(String[] args) {
+		try {
+			testa();
+		} catch (DBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void testa() throws DBException {
+		UsuarioDAO dao = DAOFactory.getUsuarioDAO();
+		
+		LocalDateTime ultimoLogin = DateUtil.toDateTime("1967-11-19T16:00"); 
+		Login login = new Login("joaogrosa@gmail.com", "grandesertaoveredas", ultimoLogin);
+		Usuario usuario = new Usuario(
+				"João Guimarães Rosa",
+				DateUtil.toDate("1908-06-27"),
+				"M",
+				new BigDecimal(1.7),
+				new BigDecimal(2000),
+				login);
+		
+		dao.excluiTodos();
+		dao.insere(usuario);
+		
+		System.out.println("Lista de alimentações registradas:");
+		dao.buscaTodos().forEach(u -> System.out.println(u));
+	}
+
+}
